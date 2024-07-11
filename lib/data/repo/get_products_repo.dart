1cadc7472ec9a9a4c290748be5680d7de7ca5fd8
youssefhedefa <78417605+youssefhedefa@users.noreply.github.com>
2024-07-11 20:01:50 +0300

    Create product entity

---
+++ lib/data/repo/get_products_repo.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:route_task/core/networking/api_manager/api_services.dart';
+import 'package:route_task/data/model/product_model.dart';
+
+class GetProductsRepo{
+  final ApiServices apiServices;
+
+  GetProductsRepo({required this.apiServices});
+
+}
---
+++ lib/domain/product_entity.dart
@@ -0,0 +1,12 @@
+class ProductEntity{
+  final int id;
+  final String title;
+  final String description;
+  final String image;
+  final String price;
+  final String discountPercentage;
+  final String rating;
+
+  ProductEntity({required this.id, required this.title, required this.description, required this.image, required this.price, required this.discountPercentage, required this.rating});
+
+}
